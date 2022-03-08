7621c9d47e866b345d12c07869f8eff51e70e1c4
sudo-nick16 <singhnikit16@gmail.com>
2022-03-08 21:02:07 +0530

    chore: profile update controllers added

--- packages/server/src/controllers/userControllers.js
+++ packages/server/src/controllers/userControllers.js
@@ -1,5 +1,9 @@
 import User from "../models/User.js";
 import { createAccessToken } from "../utils/authTokens.js";
+import validator from "validator";
+import { hashPassword } from "../utils/hashPassword.js";
+import bcryptjs from "bcryptjs";
+import { setCookies } from "../utils/setCookies.js";
 
 export const me = async (req, res) => {
   const u = req.user;
@@ -22,5 +26,64 @@ export const me = async (req, res) => {
 };
 
 export const updateMe =  async (req, res) => {
-  // const {}
+  const {name, email} = req.body;
+  const {_id} = req.user;
+  const user = await User.findById(_id).exec();
+  if(validator.isEmail(email)){
+    user.email = email;
+  }else{
+    return res.status(400).json({
+      error: "Invalid email",
+    });
+  }
+  try{
+    if(user){
+      user.name = name;
+      // user.mobile_number = phone;
+      user.email_verified = false;
+      await user.save();
+      res.status(200).json({
+        accessToken: createAccessToken(user),
+        msg: "User updated successfully",
+      });
+    }else{
+      throw new Error("User does not exist");
+    }
+  }catch(err){
+    res.status(400).json({
+      error: "Error updating user",
+    });
+  }
+}
+
+export const updatePassword = async (req, res) => {
+  const {password, oldPass} = req.body;
+  const {_id} = req.user;
+  const user = await User.findById(_id).exec();
+  try{
+    if(user){
+      const isAuth = await bcryptjs.compare(oldPass, user.password);
+      if(!isAuth){
+        throw new Error("Old password is incorrect");
+      }
+      user.password = await hashPassword(password);
+      user.token_version = user.token_version + 1;
+      await user.save();
+      setCookies(res, user);
+      res.status(200).json({
+        accessToken: createAccessToken(user),
+        msg: "Password updated successfully",
+      });
+    }else{
+      throw new Error("User does not exist");
+    }
+  }catch(err){
+    res.status(400).json({
+      error: "Error updating password",
+    });
+  }
+}
+
+export const addAddress = async (req, res) => {
+
 }
