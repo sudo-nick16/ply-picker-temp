78122659c9bcfa9692eefa5aca0c72ed28c9b735
Kshitiz Agrawal <kshitizagrawal@outlook.com>
2022-03-09 13:44:33 +0530

    add Triangle

---
+++ packages/web/src/components/Triangle/Triangle.js
@@ -0,0 +1,3 @@
+const Triangle = ({ color }) => <div style={{ width: 0, height: 0, borderTop: '10px solid transparent', borderBottom: '10px solid transparent', borderRight: `10px solid ${color}` }}></div>;
+
+export default Triangle
