4346a65f2c92740f351f76f4b30274b9915a86ee
sudo-nick16 <singhnikit16@gmail.com>
2022-03-11 00:17:59 +0530

    feat: add user to the global state on startup

--- packages/web/src/App.js
+++ packages/web/src/App.js
@@ -2,13 +2,15 @@ import { useEffect, useState } from "react";
 
 import { API_URL } from "./constants";
 import Routes from "./utils/Routes";
-import { logout, setUser } from "./store/reducers/userReducer";
+import { logout, setUser, setAuth } from "./store/reducers/userActions";
 import { useStore } from "./store/store";
 import axios from "axios";
+import useAxios from "./utils/useAxios";
 
 function App() {
   const [, dispatch] = useStore();
   const [loading, setLoading] = useState(true);
+  const api = useAxios();
 
   useEffect(async () => {
     try {
@@ -22,7 +24,12 @@ function App() {
 
       if (!response.data.error && response.data.accessToken) {
         // console.log("refreshToken fetched");
-        dispatch(setUser(response.data.accessToken, true));
+        dispatch(setAuth(response.data.accessToken, true));
+        const res = await api.get("/me");
+        console.log("res", res.data);
+        if (!res.data.error) {
+          dispatch(setUser(res.data.user));
+        }
       } else {
         console.log(response.data.error);
         dispatch(logout());
@@ -30,7 +37,6 @@ function App() {
     } catch (err) {
       console.log(err);
     }
-
     setLoading(false);
   }, []);
 
